a93d52a5d370984c7a4826d7a75e7d36912b1734
vonals <2658164762@qq.com>
2019-11-08 13:14:19 +0800

    wordbook1.0

---
+++ app/src/main/java/com/example/wordbook/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.wordbook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private String[] data = {"暹罗猫", "布偶猫", "折耳猫", "短毛猫", "波斯猫", "蓝猫", "森林猫", "孟买猫","缅因猫","埃及猫","伯曼猫","缅甸猫","新加坡猫","美国短尾猫","巴厘猫"};
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_FULL_SENSOR);
+        setContentView(R.layout.activity_main);
+        ArrayAdapter<String> adapter=new ArrayAdapter<String>(MainActivity.this,android.R.layout.simple_list_item_1,data);
+        ((ListView)findViewById(R.id.list)).setAdapter(adapter);
+    }
+    @Override
+    public void onClick(View v){
+
+    }
+}
