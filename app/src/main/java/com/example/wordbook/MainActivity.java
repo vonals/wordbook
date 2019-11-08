6d4c20435aa8aae3757af79cac6c090982ae2f2f
vonals <2658164762@qq.com>
2019-11-08 13:37:39 +0800

    1.0界面+数据库的实验

--- app/src/main/java/com/example/wordbook/MainActivity.java
+++ app/src/main/java/com/example/wordbook/MainActivity.java
@@ -10,6 +10,7 @@ import android.widget.ListView;
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
     private String[] data = {"暹罗猫", "布偶猫", "折耳猫", "短毛猫", "波斯猫", "蓝猫", "森林猫", "孟买猫","缅因猫","埃及猫","伯曼猫","缅甸猫","新加坡猫","美国短尾猫","巴厘猫"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
